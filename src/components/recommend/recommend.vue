b4c4066ee760cc7b08465411c4009464d1bb1997
dj940212 <2902273280@qq.com>
2017-06-14 22:58:05 +0800

    完成轮播图图片排布

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,28 +1,99 @@
 <template lang="html">
-    <div class="">
-        我是推荐页
+    <div class="recommend">
+        <div class="recommend-content">
+            <div class="slider-wrapper" v-if="recommends.length">
+                <slider>
+                    <div v-for="item in recommends">
+                        <a :href="item.linkUrl">
+                            <img :src="item.picUrl"/>
+                        </a>
+                    </div>
+                </slider>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-import { getRecommend } from 'api/recommend'
-import { ERR_OK } from 'api/config'
+import {
+  getRecommend
+} from 'api/recommend'
+import {
+  ERR_OK
+} from 'api/config'
+import Slider from 'src/base/slider/slider'
 export default {
   created() {
     this._getRecommend()
+  },
+  data() {
+    return {
+      recommends: []
+    }
   },
   methods: {
     _getRecommend() {
       getRecommend().then((res) => {
         if (res.code === ERR_OK) {
           console.log(res.data.slider)
+          this.recommends = res.data.slider
         }
       })
     }
+  },
+  components: {
+    Slider
   }
 }
 </script>
 
-<style lang="stylus">
-    @import "~common/stylus/variable"
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+
+  .recommend
+    position: fixed
+    width: 100%
+    top: 88px
+    bottom: 0
+    .recommend-content
+      height: 100%
+      overflow: hidden
+      .slider-wrapper
+        position: relative
+        width: 100%
+        overflow: hidden
+      .recommend-list
+        .list-title
+          height: 65px
+          line-height: 65px
+          text-align: center
+          font-size: $font-size-medium
+          color: $color-theme
+        .item
+          display: flex
+          box-sizing: border-box
+          align-items: center
+          padding: 0 20px 20px 20px
+          .icon
+            flex: 0 0 60px
+            width: 60px
+            padding-right: 20px
+          .text
+            display: flex
+            flex-direction: column
+            justify-content: center
+            flex: 1
+            line-height: 20px
+            overflow: hidden
+            font-size: $font-size-medium
+            .name
+              margin-bottom: 10px
+              color: $color-text
+            .desc
+              color: $color-text-d
+      .loading-container
+        position: absolute
+        width: 100%
+        top: 50%
+        transform: translateY(-50%)
 </style>
