bafc6d22def2dc0b1bcf6576ad2b5b362007f1d2
dj940212 <2902273280@qq.com>
2017-06-29 10:45:01 +0800

    首次

--- src/store/mutations.js
+++
@@ -1,9 +0,0 @@
-import * as types from './mutation-types'
-
-const mutations = {
-    [types.SET_SINGER](state, singer){
-        state.singer = singer
-    }
-}
-
-export default mutations
--- src/store/state.js
+++
@@ -1,5 +0,0 @@
-const state = {
-    singer: {}
-}
-
-export default state
